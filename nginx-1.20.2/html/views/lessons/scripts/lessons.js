4cea57b4edea3f8bb188adeb7aabf0eae774a0c0
Carlos Lopez <carlos.alopezfigueroa@gmail.com>
2022-04-27 21:46:26 -0600

    complete search and filter of lessons

--- nginx-1.20.2/html/views/lessons/scripts/lessons.js
+++ nginx-1.20.2/html/views/lessons/scripts/lessons.js
@@ -4,19 +4,51 @@ import { debounce } from '../../../shared/scripts/debounce.js';
 
 const listLessonsDiv = document.querySelector('.list-lessons');
 const searchLessons = document.querySelector('#lessons-search');
-searchLessons.addEventListener('keyup', debounce((event) => searchByName(event), 1000));
+const filterSelect = document.querySelector('#category');
+searchLessons.addEventListener('keyup', debounce((event) => searchByName(event.target.value), 1000));
+filterSelect.addEventListener('change', (event) => filterLessons(event.target.value));
 
 async function loadListLessons() {
   const lessonsInfo = await lessonServiceInit.getLessons();
+  const categories = new Set();
+  lessonsInfo.forEach((lesson) => {
+    lesson.category.forEach((category) => {
+      categories.add(category);
+    });
+  });
+  lessonServiceInit.categories = categories;
   renderLessons(lessonsInfo);
+  renderOptions();
 };
 
 loadListLessons();
 
-function searchByName(event) {
-  const query = event.target.value.toLowerCase();
-  const newList = lessonServiceInit.lessonsCache.filter((lesson) => lesson.title.toLowerCase().includes(query));
-  renderLessons(newList);
+function searchByName(queryValue, preFilterList) {
+  const query = queryValue.toLowerCase();
+  let newList;
+  if (!preFilterList) {
+    newList = lessonServiceInit.lessonsCache.filter((lesson) => lesson.title.toLowerCase().includes(query));
+    filterLessons(filterSelect.value, newList);
+  } else {
+    newList = preFilterList.filter((lesson) => lesson.title.toLowerCase().includes(query));
+    renderLessons(newList);
+  }
+}
+
+function filterLessons(filterValue, preSearchList) {
+  const selectedFilter = filterValue;
+  let newList;
+  if (!preSearchList) {
+    newList = selectedFilter === 'todas' ? 
+    lessonServiceInit.lessonsCache : 
+    lessonServiceInit.lessonsCache.filter((lesson) => lesson.category.map((category) => category.toLowerCase()).includes(selectedFilter));
+    searchByName(searchLessons.value, newList);
+  } else {
+    newList = selectedFilter === 'todas' ? 
+    preSearchList : 
+    preSearchList.filter((lesson) => lesson.category.map((category) => category.toLowerCase()).includes(selectedFilter));
+    renderLessons(newList);
+  }
 }
 
 function renderLessons(lessons) {
@@ -25,5 +57,16 @@ function renderLessons(lessons) {
   lessons.forEach((lessonData) => {
     const newLesson = lesson(lessonData);
     listLessonsDiv.appendChild(newLesson.template);
+  });
+};
+
+function renderOptions() {
+  const select = document.querySelector('#category')
+  const lessonsCategories = lessonServiceInit.categories;
+  lessonsCategories.forEach((option) => {
+    const newOption = document.createElement('option');
+    newOption.value = option.toLowerCase();
+    newOption.innerText = option;
+    select.appendChild(newOption);
   });
 }
